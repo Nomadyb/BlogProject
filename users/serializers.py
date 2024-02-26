952e28f98caa1de9b8d11b38bd27ffe718262790
Nomadyb <Nomadaeyb@outlook.com>
2024-02-26 11:18:42 +0300

    regex yapısı kullanarak  validate tamamlandı

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import User
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 import re 
 
 
@@ -16,67 +17,6 @@ amaç rest api üzerinden kullanıcı bilgilerini almak ve kaydetmek(JSON format
 # TODO: role yapısını charfield olarak alma
 
 
-
-# class UserSerializer(serializers.Serializer):
-#     # permission_classes = (AllowAny,)
-#     # def __init__(self,id,username,email,role):
-#     id = serializers.IntegerField(read_only=True)
-#     username = serializers.CharField(max_length=100)
-#     email = serializers.EmailField(max_length=70)
-#     role = serializers.CharField(max_length=100)
-
-#     # verilerin doğrulanmasına bağlı olarak nesnelerin döndürülmesi için hem create hem update uygulanmalıdır
-#     def create(self, validated_data):
-#         return User.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         instance.username = validated_data.get('username', instance.username)
-#         instance.email = validated_data.get('email', instance.email)
-#         instance.role = validated_data.get('role', instance.role)
-#         instance.save()
-#         return instance
-
-#     # bir validate yazmalısın
-
-
-
-
-# class UserSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     username = serializers.CharField(max_length=100, required=False)
-#     email = serializers.EmailField(max_length=70)
-#     role = serializers.CharField(max_length=100, required=False)
-
-#     password = serializers.CharField(
-#         max_length=128, write_only=True)  # Yeni eklenen password alanı
-
-#     def create(self, validated_data):
-#         return User.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         instance.username = validated_data.get('username', instance.username)
-#         instance.email = validated_data.get('email', instance.email)
-#         instance.role = validated_data.get('role', instance.role)
-#         instance.save()
-#         return instance
-
-#     def validate(self, attrs):
-#         email = attrs.get('email')
-#         password = attrs.get('password')
-
-#         if not email:
-#             raise serializers.ValidationError(
-#                 {'email': 'This field is required.'})
-#         if not password:
-#             raise serializers.ValidationError(
-#                 {'password': 'This field is required.'})
-
-#         return attrs
-
-
-from rest_framework import serializers
-import re
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -104,23 +44,21 @@ class UserSerializer(serializers.Serializer):
         email = attrs.get('email')
         password = attrs.get('password')
 
-        # E-posta formatını kontrol et
+        #re
         # if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
         if not re.match(r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", email):
             raise serializers.ValidationError("Invalid email format")
 
+
+        if not re.match(r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_+=])[a-zA-Z0-9!@#$%^&*()-_+=]{8,}$", password):
+            raise serializers.ValidationError("Invalid password format")
+            
         return attrs
 
 
 
-# import re
 
 
 
 
-#     def validate_email(self, value):
-#         # E-posta formatını kontrol et
-#         if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
-#             raise serializers.ValidationError("Invalid email format")
-#         return value
 
