3a6cb89e50ff1dc20a915d2773cff8dd7cb9e1a5
Nomadyb <Nomadaeyb@outlook.com>
2024-02-26 14:25:49 +0300

    views.py file is modified and we are going to commit the changes(old register and login deleted).

--- users/views.py
+++ users/views.py
@@ -25,80 +25,7 @@ import logging
 # TODO: veritabanında token tutma django jwt token  kullan
 
 
-# class RegisterView(APIView):
-#     permission_classes = [AllowAny]
 
-#     def post(self, request):
-#         try:
-#             username = request.data.get('username')
-#             email = request.data.get('email')
-#             password = request.data.get('password')
-#             role = request.data.get('role')
-
-#             if not (username and email and password):
-#                 return Response({
-#                     'isSuccess': False,
-#                     'message': 'Registration failed',
-#                     'errors': {'detail': 'Username, email, and password are required.'}
-#                 }, status=status.HTTP_400_BAD_REQUEST)
-
-#             if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
-#                 return Response({
-#                     'isSuccess': False,
-#                     'message': 'Registration failed',
-#                     'errors': {'detail': 'Username or email is already taken.'}
-#                 }, status=status.HTTP_400_BAD_REQUEST)
-
-#             # Kullanıcı oluştur
-#             user = User.objects.create(
-#                 username=username,
-#                 email=email,
-#                 password=make_password(password),
-#                 role=role
-#             )
-#             user.is_active = True
-#             user.save()
-
-#             # # JWT ayarlarını al
-#             # jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-#             # jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-#             # # JWT token oluştur
-#             # payload = jwt_payload_handler(user)
-#             # token = jwt_encode_handler(payload)
-
-#             # Token objesini kaydet
-#             # Token.objects.create(user=user)
-
-#             user_data = {
-#                 'id': user.id,
-#                 'username': user.username,
-#                 'email': user.email,
-#                 'role': user.role,
-#                 'date_joined': user.date_joined.strftime('%Y-%m-%d %H:%M:%S'),
-#                 'is_active': user.is_active,
-#             }
-
-#             # Log kaydı tutma
-#             logger = logging.getLogger(__name__)
-#             logger.info(f'New user registered: {user.username}')
-
-#             return Response({
-#                 'isSuccess': True,
-#                 'message': 'Registration successful',
-#                 'data': user_data,
-#                 # 'token': token
-#             }, status=status.HTTP_201_CREATED)
-#         except Exception as e:
-#             # Hata durumunda log kaydı tutma
-#             logger = logging.getLogger(__name__)
-#             logger.error(f'An error occurred during registration: {str(e)}')
-
-#             return Response({
-#                 'isSuccess': False,
-#                 'message': 'An error occurred during registration',
-#                 'error': str(e),
-#             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 from rest_framework.response import Response
@@ -193,34 +120,7 @@ class LoginView(APIView):
 
 
 
-# class LoginView(APIView):
-#     def post(self, request):
-#         email = request.data.get('email')
-#         password = request.data.get('password')
 
-
-#         user = authenticate(request, email=email, password=password)
-#         #TODO:
-#         if user:
-#             #data
-#             user_data = {
-#                 "id":user.id,
-#                 "username": user.username,
-
-
-#             }
-
-
-#             jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-#             jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-#             payload = jwt_payload_handler(user)
-#         else:
-
-#             token = jwt_encode_handler(payload)
-
-#             return Response({'token': token}, status=status.HTTP_200_OK)
-#         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class HomeView(APIView):
